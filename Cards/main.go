ffe35e579e6183b86f4c851b4bc86f712487c39f
Rohan Kumar <rohankumarmenu@gmail.com>
2026-05-18 11:56:26 +0530

    New change

---
+++ Cards/main.go
@@ -0,0 +1,45 @@
+package main
+
+// func main() {
+// 	println("Hello, World!")
+
+// 	//var card string = "Ace of Spades"
+// 	card := "Ace of Spades"   //initialization
+// 	card = "Five of Diamonds" //reassignment of value
+// 	fmt.Println(card)
+// }
+
+//   ------
+
+// func main() {
+// 	card := newCard()
+
+// 	fmt.Println(card)
+// }
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
+
+// func main() {
+// 	cards := deck{"Ace of Diamonds", newCard()}
+// 	cards = append(cards, "Six of Spades")
+
+// 	// for i, card := range cards {
+// 	// 	fmt.Println(i, card)
+// 	// }
+
+// 	cards.print()
+// }
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
+
+func main() {
+	cards := newDeck()
+	//cards.print()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
+}
