c8af944697c238fdd0da0f357b67bc3d4c374b86
Rohan Kumar <rohankumarmenu@gmail.com>
2026-05-28 11:56:33 +0530

    UpdatingStructs

--- Structs/main.go
+++ Structs/main.go
@@ -8,6 +8,13 @@ type person struct {
 }
 
 func main() {
-	alex := person{firstName: "Rahul", lastName: "Dravid"}
+	// alex := person{firstName: "Rahul", lastName: "Dravid"}
+	// fmt.Println(alex)
+
+	var alex person
+	alex.firstName = "Virendra"
+	alex.lastName = "Sehwag"
 	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+
 }
