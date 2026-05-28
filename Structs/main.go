fd332e4835e94a35b7f60a9febdb64b2931b64d1
Rohan Kumar <rohankumarmenu@gmail.com>
2026-05-28 13:30:03 +0530

    StructsWithReceiverFunction

--- Structs/main.go
+++ Structs/main.go
@@ -5,16 +5,40 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
 }
 
 func main() {
 	// alex := person{firstName: "Rahul", lastName: "Dravid"}
 	// fmt.Println(alex)
 
-	var alex person
-	alex.firstName = "Virendra"
-	alex.lastName = "Sehwag"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	// var alex person
+	// alex.firstName = "Virendra"
+	// alex.lastName = "Sehwag"
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
 
+	check := person{
+		firstName: "Virendra",
+		lastName:  "Sehwag",
+		contactInfo: contactInfo{
+			email:   "sehwag@example.com",
+			zipCode: 12345,
+		},
+	}
+	check.updatedName("Veeru")
+	check.printed()
+}
+
+func (p person) printed() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updatedName(newFirstName string) {
+	p.firstName = newFirstName
 }
