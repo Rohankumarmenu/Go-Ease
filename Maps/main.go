ddc8029f186e1efd1028f23411fd41a5ecbb66da
Rohan Kumar <rohankumarmenu@gmail.com>
2026-05-30 11:33:03 +0530

    IterationOfMap

--- Maps/main.go
+++ Maps/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// colores := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#00ff00",
-	// }
-	colores := make(map[int]string)
-	colores[10] = "#ff0000"
-	delete(colores, 10)
-	fmt.Println(colores)
+	colores := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+	// colores := make(map[int]string)
+	// colores[10] = "#ff0000"
+	// delete(colores, 10)
+	//fmt.Println(colores)
+	printMap(colores)
 
+}
+
+func printMap(m map[string]string) {
+	for col, hex := range m {
+		fmt.Println("Hex code for", col, "is", hex)
+	}
 }
